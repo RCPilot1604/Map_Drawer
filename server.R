ed80fbde163bc4f8ce5e2250b1a077ab1f0f7fcd
RCPilot1604 <66578794+RCPilot1604@users.noreply.github.com>
2021-01-30 17:48:22 +0800

    Initial commit

---
+++ server.R
@@ -0,0 +1,70 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny);
+library(ggplot2);
+library(dplyr);
+library(DT);
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+
+    output$upload <- renderUI({
+       fileInput("CSVIn", "Upload your .csv file here", multiple = TRUE, accept = c(
+           "text/csv",
+           "text/comma-separated-values,text/plain",
+           ".csv"), width = NULL, buttonLabel = "Browse...", placeholder = "No file selected")
+   })
+    output$AdjMatrix_output <- renderUI({
+        if(is.null(input$CSVIn)){return(helpText("Please input CSV!"));}
+        selectInput("dataSelect_Adj","Select your Adjacency Matrix from list of data",input$CSVIn$name)
+    })
+    output$Coord_output <- renderUI({
+        if(is.null(input$CSVIn)){return();}
+        selectInput("dataSelect_Coord","Select your X_Coordinates from list of data",input$CSVIn$name)
+    })
+    
+    output$x_col_output <- renderUI({
+        if(is.null(input$CSVIn)){return();}
+        selectInput("x_col","Choose the x column", colnames(Coord_input()));
+    })
+    output$y_col_output <- renderUI({
+        if(is.null(input$CSVIn)){return();}
+        selectInput("y_col","Choose the x column", colnames(Coord_input()));
+    })
+
+    AdjMatrix <- reactive({
+        if(is.null(input$CSVIn)){return();}
+        upload <- input$CSVIn$datapath[input$CSVIn$name==input$dataSelect_Adj] %>% read.csv();
+        upload[is.na(upload)] <- 0; #Replace all the NAs with 0
+        return(upload);
+    })
+    
+    Coord_input <- reactive({
+        if(is.null(input$CSVIn)){return();}
+        upload <- input$CSVIn$datapath[input$CSVIn$name==input$dataSelect_Coord] %>% read.csv();
+        upload[is.na(upload)] <- 0; #Replace all the NAs with 0
+        return(upload);
+    })
+    
+    
+    output$data_adj <- DT::renderDataTable({ 
+        if(is.null(input$CSVIn)){return();}
+        DT::datatable(AdjMatrix())
+    })
+    output$Coord <- DT::renderDataTable({ 
+        if(is.null(input$CSVIn)){return();}
+        DT::datatable(Coord_input())
+    })
+    output$plot <- renderPlot({
+        data <- Coord_input()
+        gg <- ggplot(data = data, mapping = aes_string(x= input$x_col, y= input$y_col)) + geom_point()
+        return(gg)
+    })
+})
---
+++ ui.R
@@ -0,0 +1,47 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(DT)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+
+    # Application title
+    titlePanel("Bharat EE"),
+
+    # Sidebar with a slider input for number of bins
+    sidebarLayout(
+        sidebarPanel(
+            uiOutput("upload"),
+            uiOutput("AdjMatrix_output"),
+            uiOutput("Coord_output"),
+            uiOutput("x_col_output"),
+            uiOutput("y_col_output")
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+            tabsetPanel(
+                tabPanel(
+                    "Data",
+                    DT::dataTableOutput("data_adj"), 
+                    DT::dataTableOutput("Coord"), 
+                    helpText("Here is your data!")
+                ),
+                tabPanel(
+                    "Plot",
+                    plotOutput("plot"),
+                    helpText("Here is your plot")
+                )
+            
+            ),
+        )
+    )
+))
